97b271adfa695fdd9e208c0284db4a9beb12c4ed
Agustin Meliendrez <ameliendrez@gmail.com>
2020-02-20 22:39:02 -0300

    Changes in proyect

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -3,10 +3,10 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Mi primer componente</div>
+                    <div class="card-header">Mi primer componente con Vue JS</div>
 
                     <div class="card-body">
-                        Soy un ejemplo de vue js
+                        Soy un ejemplo de vue.js, laravel y docker
                     </div>
                 </div>
             </div>
